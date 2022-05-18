7c191cf60fe0d3a31aeb0ba7983b664a256a8b47
Spakelios <73300254+Spakelios@users.noreply.github.com>
2022-05-18 17:59:45 +0100

    title screen

--- itsAGame/Assets/buttons.cs
+++ itsAGame/Assets/buttons.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -20,5 +21,10 @@ public class buttons : MonoBehaviour
         public void healButton()
         {
             giuseppePriority.Heal();
+        }
+
+        public void OnApplicationQuit()
+        { 
+            Application.Quit();
         }
 }
