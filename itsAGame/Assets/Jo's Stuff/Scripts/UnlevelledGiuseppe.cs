a5873651fada2c8ab3bfb9f2b2d7e2e5288d38f8
Spakelios <73300254+Spakelios@users.noreply.github.com>
2022-05-19 01:59:06 +0100

    fixed

--- itsAGame/Assets/Jo's Stuff/Scripts/UnlevelledGiuseppe.cs
+++ itsAGame/Assets/Jo's Stuff/Scripts/UnlevelledGiuseppe.cs
@@ -71,6 +71,7 @@ public class UnlevelledGiuseppe : MonoBehaviour
 
             else if (numberKills < numberHeals)
             {
+                Destroy(enemyDetect);
                 healerLevel.enabled = true;
             }
         }
